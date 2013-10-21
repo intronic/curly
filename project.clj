9d8daa02b3ce28607dd45f7a25f610308960f835
Michael Pheasant <mpheasant@gmail.com>
2013-10-21 12:51:12 +1000

    add github path

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject intronic/curly "0.1.0-SNAPSHOT"
   :description "Clojure utility library"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/intronic/curly"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
