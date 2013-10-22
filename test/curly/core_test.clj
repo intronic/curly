61dd5fee9124d93d24ba4640ac5628398aa71abc
Michael Pheasant <mpheasant@gmail.com>
2013-10-22 15:34:45 +1000

    add some tests

--- test/curly/core_test.clj
+++ test/curly/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [curly.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest numeric
+  (testing "one?"
+    (is (one? 1))
+    (is (not (one? 0)))
+    (is (not (one? 2))))
+  (testing "nan?"
+    (is (nan? (/ 0 0.0)))))
+
+(deftest stats
+  (testing "z-score"
+    (is (zero? (z-score 0)))
+    (is (zero? (z-score 0 {:mean 0 :sd 1})))
+    (is (zero? (z-score 0 {:mean 0 :sd 100})))
+    (is (= 1.0 (z-score 1 {:mean 0 :sd 1})))
+    (is (= 2.0 (z-score 2 {:mean 0 :sd 1})))
+    (is (zero? (z-score 1 {:mean 1 :sd 2})))))
