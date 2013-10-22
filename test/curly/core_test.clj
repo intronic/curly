f1ea2ca646347c9d32c731b127cc23bff8d9082a
Michael Pheasant <mpheasant@gmail.com>
2013-10-22 15:41:48 +1000

    strings test

--- test/curly/core_test.clj
+++ test/curly/core_test.clj
@@ -18,3 +18,12 @@
     (is (= 1.0 (z-score 1 {:mean 0 :sd 1})))
     (is (= 2.0 (z-score 2 {:mean 0 :sd 1})))
     (is (zero? (z-score 1 {:mean 1 :sd 2})))))
+
+(deftest strings
+  (testing "string-splice"
+    (is (= "abobe" (string-splice "abcde" "bob" 1)))
+    (is (= "bobde" (string-splice "abcde" "bob" 0)))
+    (is (= "12345" (string-splice "abcde" "12345" 0))))
+  (testing "starts-with-ignore-case?"
+    (is (starts-with-ignore-case? "abcde" "aBc"))
+    (is (not (starts-with-ignore-case? "abcde" "bc")))))
