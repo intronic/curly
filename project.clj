0001a5ce23fe0f8a6f95e2b6128797cbfeb78f03
Michael Pheasant <mpheasant@gmail.com>
2013-10-22 16:38:24 +1000

    update version

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject intronic/curly "0.1.1"
+(defproject intronic/curly "0.1.2"
   :description "Clojure utility library"
   :url "https://github.com/intronic/curly"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :scm {:name "git"
-        :url "https://github.com/intronic/curly"})
+        :url "https://github.com/intronic/curly"
+        :tag ""})
